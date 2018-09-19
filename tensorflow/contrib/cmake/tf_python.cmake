40b8dd94cb7bbdf773f00114bf046030aec0d4ec
ShinYee <shinyee@speedgocomputing.com>
2018-09-19 21:07:12 +0800

    posit: add posit8: update makefiles.
    Signed-off-by: ShinYee <shinyee@speedgocomputing.com>

--- tensorflow/contrib/cmake/tf_python.cmake
+++ tensorflow/contrib/cmake/tf_python.cmake
@@ -484,6 +484,8 @@ set (pywrap_tensorflow_internal_src
     "${tensorflow_source_dir}/tensorflow/python/framework/python_op_gen_internal.cc"
     "${tensorflow_source_dir}/tensorflow/python/lib/core/bfloat16.h"
     "${tensorflow_source_dir}/tensorflow/python/lib/core/bfloat16.cc"
+    "${tensorflow_source_dir}/tensorflow/python/lib/core/posit8.h"
+    "${tensorflow_source_dir}/tensorflow/python/lib/core/posit8.cc"
     "${tensorflow_source_dir}/tensorflow/python/lib/core/posit16.h"
     "${tensorflow_source_dir}/tensorflow/python/lib/core/posit16.cc"
     "${tensorflow_source_dir}/tensorflow/python/lib/core/posit32.h"
